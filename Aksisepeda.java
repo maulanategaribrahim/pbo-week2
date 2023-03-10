4dd142207161a2806d48e521bb90df5b2eb230bd
MAULANA TEGAR IBRAHIM <88541166+maulanategaribrahim@users.noreply.github.com>
2023-03-10 23:53:45 +0700

    Add files via upload

---
+++ Aksisepeda.java
@@ -0,0 +1,11 @@
+public class Aksisepeda { 
+	public static void main(String[] args) { 
+	 //membuat objek
+	 Sepeda sepedaBalap = new Sepeda(); 
+	 
+	 //mengakses atribut dan method
+	 int gearSepeda = sepedaBalap.gear; 
+	 System.out.println(gearSepeda); 
+	 sepedaBalap.ngerem(); 
+	 } 
+} 
---
+++ Sepeda.java
@@ -0,0 +1,9 @@
+public class Sepeda{
+  int gear=5;
+  
+  void ngerem(){
+  System.out.println("Sepeda direm");
+  }
+
+
+}
